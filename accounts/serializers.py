d1a41d204b865f5c1048792c49522c9d773f69d2
mahdi <mahdi.ramezani.059@gmail.com>
2025-04-26 14:44:22 +0330

    remove unnecessary comments from accounts/serializers.py and accounts/urls.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -20,20 +20,4 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('password must match')
         return data
 
-# class RegisterSerializer(serializers.ModelSerializer):
-#     password_check = serializers.CharField(write_only=True , required=True)
-#     class Meta:
-#         model = User
-#         fields = ('username' , 'email' , 'password' , 'password_check')
-#         extra_kwargs = {
-#             'password': {'write_only': True},
-#         }
 
-#     def create(self, validated_data):
-#         del validated_data['password_check']
-#         return User.objects.create_user(**validated_data)
-
-#     def validate(self , data):
-#         if data['password'] != data['password_check']:
-#             raise serializers.ValidationError('password must match')
-#         return data
